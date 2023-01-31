740bedfbd9a52e0451fd251358aa315b8104308b
kuntzloic <kuntz.loic57@gmail.com>
2023-01-31 16:09:38 +0100

    Sharpen rendering

--- main.js
+++ main.js
@@ -1,26 +1,31 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.
-    PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer(
-
-);
+const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({
+    antialias: true,
+});
 
 renderer.setSize(innerWidth, innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
-const sphere = new THREE.Mesh(new THREE.SphereGeometry(5, 50, 50), new THREE.MeshBasicMaterial({
-   // color: 0xFF0000
-    map: new THREE.TextureLoader().load("./img/globe.jpeg")
-}));
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(5, 50, 50),
+    new THREE.MeshBasicMaterial({
+            // color: 0xFF0000
+            map: new THREE.TextureLoader().load("./img/globe.jpeg")
+        }
+    )
+);
 
 scene.add(sphere)
 
-camera.position.z = 20;
+camera.position.z = 15;
 
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 }
+
 animate();
