db5120514795148373415c4f134e12b92fe09722
kuntzloic <kuntz.loic57@gmail.com>
2023-01-31 21:48:33 +0100

    setup of basic shaders

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 import * as THREE from 'three';
+import vertexShader from './shaders/vertex.glsl'
+import fragmentShader from './shaders/fragment.glsl'
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
@@ -12,9 +14,9 @@ document.body.appendChild(renderer.domElement);
 
 const sphere = new THREE.Mesh(
     new THREE.SphereGeometry(5, 50, 50),
-    new THREE.MeshBasicMaterial({
-            // color: 0xFF0000
-            map: new THREE.TextureLoader().load("./img/globe.jpeg")
+    new THREE.ShaderMaterial({
+            vertexShader: vertexShader,
+            fragmentShader: fragmentShader
         }
     )
 );
